ca12f49c0137751c7d4c52df1ea6c2b08b378a43
Sam Morris <rc0033468@student.riversidecollege.ac.uk>
2018-09-18 09:23:12 +0100

    added a button and a picture of a dog

--- follow path/Form1.cs
+++ follow path/Form1.cs
@@ -16,5 +16,10 @@ namespace follow_path
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
